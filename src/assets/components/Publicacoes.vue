d63fca5113063b45435f526163cbf087016b6635
João Batista Gome Silva <jhonxbatista@gmail.com>
2019-05-16 15:22:24 -0300

    correção do erro no build

--- src/assets/components/Publicacoes.vue
+++ src/assets/components/Publicacoes.vue
@@ -27,13 +27,11 @@
 </template>
 
 <script>
-    import pdf from 'vue-pdf'
     import publications_data from '../data/publications'
     const { publications } = publications_data
     export default {
         name: "Publicacoes.vue",
         components: {
-          pdf
         },
         data() {
           return {
